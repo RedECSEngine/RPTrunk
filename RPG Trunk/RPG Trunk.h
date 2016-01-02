0e4c89d4a072cd94baa73606a9e64b8a3a53898c
kylenewsome <kyle@bitwit.ca>
2016-01-02 15:08:51 -0500

    Initial Commit

---
+++ RPG Trunk/RPG Trunk.h
@@ -0,0 +1,19 @@
+//
+//  RPG Trunk.h
+//  RPG Trunk
+//
+//  Created by Kyle Newsome on 2016-01-02.
+//  Copyright © 2016 Kyle Newsome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RPG Trunk.
+FOUNDATION_EXPORT double RPG_TrunkVersionNumber;
+
+//! Project version string for RPG Trunk.
+FOUNDATION_EXPORT const unsigned char RPG_TrunkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RPG_Trunk/PublicHeader.h>
+
+
